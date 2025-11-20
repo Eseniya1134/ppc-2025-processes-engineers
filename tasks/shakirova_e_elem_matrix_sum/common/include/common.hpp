c68be468189263f22abdd898873889ef1d267416
Eseniya1134 <shakirova.esya23@mail.ru>
2025-11-20 16:43:29 +0300

    common.hpp is ready

--- tasks/shakirova_e_elem_matrix_sum/common/include/common.hpp
+++ tasks/shakirova_e_elem_matrix_sum/common/include/common.hpp
@@ -2,14 +2,15 @@
 
 #include <string>
 #include <tuple>
+#include <vector>
 
 #include "task/include/task.hpp"
 
 namespace shakirova_e_elem_matrix_sum {
 
-using InType = int;
-using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using InType = std::vector<std::vector<int>>;
+using OutType = long long;
+using TestType = std::tuple<int, int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace shakirova_e_elem_matrix_sum
